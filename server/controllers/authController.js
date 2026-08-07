72d1ea0005c2f90a2c7bd2d5427625bcc008a18b
Hameed-Abdul06 <hameed.abdul.real@gmail.com>
2026-08-07 16:08:42 +0530

    Added authentication module structure

---
+++ server/controllers/authController.js
@@ -0,0 +1,65 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const generateToken = (id) => {
+    return jwt.sign(
+        { id },
+        process.env.JWT_SECRET,
+        { expiresIn: "7d" }
+    );
+};
+
+// Register User
+const registerUser = async (req, res) => {
+    try {
+        const { fullName, email, password } = req.body;
+
+        if (!fullName || !email || !password) {
+            return res.status(400).json({
+                message: "Please fill all fields",
+            });
+        }
+
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return res.status(400).json({
+                message: "User already exists",
+            });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const user = await User.create({
+            fullName,
+            email,
+            password: hashedPassword,
+        });
+
+        res.status(201).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            token: generateToken(user._id),
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+};
+
+// Login User (we'll implement this next)
+const loginUser = async (req, res) => {
+    res.json({
+        message: "Login API coming next",
+    });
+};
+
+module.exports = {
+    registerUser,
+    loginUser,
+};
