24c4f4fbc8bd6bc0ccd07ad8143c1f5be7de9725
hameed-abdul06 <hameed.abdul.real@gmail.com>
2026-08-10 10:40:12 +0530

    Complete authentication APIs

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -52,14 +52,64 @@ const registerUser = async (req, res) => {
     }
 };
 
-// Login User (we'll implement this next)
+// Login User
 const loginUser = async (req, res) => {
-    res.json({
-        message: "Login API coming next",
-    });
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "Please enter email and password",
+            });
+        }
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(401).json({
+                message: "Invalid email or password",
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({
+                message: "Invalid email or password",
+            });
+        }
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            token: generateToken(user._id),
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+};
+
+// Get Logged-in User Profile
+const getProfile = async (req, res) => {
+    try {
+        res.status(200).json({
+            _id: req.user._id,
+            fullName: req.user.fullName,
+            email: req.user.email,
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
 };
 
 module.exports = {
     registerUser,
     loginUser,
+    getProfile,
 };
