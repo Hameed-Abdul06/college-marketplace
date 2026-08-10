ca21e481e91b5915e1ed33d993955ea93a11ecb3
hameed-abdul06 <hameed.abdul.real@gmail.com>
2026-08-10 21:55:23 +0530

    Fix navbar styling deployment

--- client/src/components/common/Navbar.jsx
+++ client/src/components/common/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
-import "../../styles/Navbar.css";
+
 
 function Navbar() {
     const { user, logout } = useContext(AuthContext);
